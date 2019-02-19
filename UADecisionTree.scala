e491062c53aa17df297406130b22429574bead6f
rfishe02 <43683461+rfishe02@users.noreply.github.com>
2019-02-19 01:04:27 -0600

    Add files via upload

--- UADecisionTree.scala
+++ UADecisionTree.scala
@@ -2,64 +2,78 @@
 import scala.io.Source
 import scala.collection.mutable.HashMap
 import scala.collection.mutable.HashSet
-import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.ListBuffer
 
 class UADecisionTree {
 
   var depth : Integer = _;
   var impur : Float = _;
   
-  //------------------------------------------------
-  // Necessary methods
-  //------------------------------------------------
+  //=============================================================
+  // The necessary methods.
+  //=============================================================
   
-  def getTrainingMatrix(filename : String, target: String) : ArrayBuffer[ArrayBuffer[String]] = {
+  //=============================================================
+  // This method builds a training matrix from the training data.
+  //=============================================================
+  
+  def getTrainingMatrix(filename : String, target: String) = {
     
-    // Count the number of sub-attributes.
-    
-    val stats = getStats(filename) // ( count, rows, cols )
-    
-    // Rearrange the data such that the target variable is at the end.
-
-    val src = Source.fromFile(filename)
-    val itr = src.getLines()
-    
-    val labels = itr.next().split(",")
-    
-    val matrix = ArrayBuffer.fill[ArrayBuffer[String]](stats._2)(null)
-    
-    var col : Integer = 0
-    var row : Integer = 0
+    var c : Integer = 0
     var spl = Array[String]()
+    var tmp = Array[String]()
+    //var r : Integer = 0
     
-    for(line <- itr) {
-      spl = line.split(",")
-      col = 0
+    // Determine which subattributes are valid.
+    
+    val stats = getStats(filename, target) // ( data, s, c )
+    val matrix = ListBuffer.empty[Array[String]]
+    //val matrix = Array.ofDim[String](stats._1.size-1,stats._3) // Don't include attData in the matrix.
+
+    // Rearrange the data such that the target variable is at the end.
+    
+    val data = stats._1.iterator
+    val attData = data.next()
+    
+    for( item <- data ) {
+      c = 0
       
-      matrix(row) = ArrayBuffer.fill[String](stats._3)(null)
+      tmp = new Array[String](stats._3)
       
-      for(ind <- 0 to spl.length -1) {
+      for(col <- 0 to item.length - 1) {
         
-        if(stats._1(ind) <= 20) {
+        if(stats._2(col) <= 20) {
           
-          if(labels(ind).equalsIgnoreCase(target)){
-            matrix(row)(matrix(row).length-1) = spl(ind)
+          /*
+          if(attr(c) == null) {
+            attr(c) = attData(col)
+          }*/
           
+          if(attData(col).equalsIgnoreCase(target)) { 
+            //matrix(r)(matrix(r).length-1) = item(col)
+            tmp(tmp.length-1) = item(col)
+            
           } else {
-            matrix(row)(col) = spl(ind)
-          
-            col += 1
+            //matrix(r)(c) = item(col)
+            tmp(c) = item(col)
+            c += 1
           }
+          
         }
       }
-      row += 1
+      matrix += tmp
+      
+      //r += 1
     }
+
+    //printData(matrix)
     
-    printMatrix(matrix)
-    
-    return matrix
-    
+    (matrix)
   }
+  
+  //=============================================================
+  // These are the setters for detph & impurity.
+  //=============================================================
   
   def setTreeMaxDepth(max : Integer) {
     depth = max
@@ -71,7 +85,11 @@ class UADecisionTree {
     
   }
   
-  def train(data : Iterator[ArrayBuffer[String]], d : Integer) {
+  //=============================================================
+  // This is the train method.
+  //=============================================================
+  
+  def train(data : Iterator[Array[String]], d : Integer) {
     
     // Must take into account max tree limit &
     // min impurity value.
@@ -83,13 +101,6 @@ class UADecisionTree {
       // 1. Find the attribute with the largest IG. 
       //    this becomes node current.
       
-      var maxCol : Integer = 0
-      
-      for(item <- data) {
-        
-        
-      }
-      
       // 2. Node current becomes an inner node.
       //    Split the results on this value & create child nodes.
       
@@ -97,23 +108,15 @@ class UADecisionTree {
       
       // 4. If the subset is pure, do not split. Otherwise,
       //    continue to recursively split the results on the child node.
-      
-      var subSet = getSubSet(data,maxCol)
-      
-      for( item <- subSet) {
-        train(item.iterator,d+1);
-        
-      }
+
       
     }
     
   }
   
-  def calcEntropy(data : Iterator[ArrayBuffer[String]]) = {
-    
-    
-    
-  }
+  //=============================================================
+  // This method is used to test the application.
+  //=============================================================
   
   def classifyValue(record : String) {
     
@@ -121,101 +124,216 @@ class UADecisionTree {
     
   }
   
-  //------------------------------------------------
-  // My methods
-  //------------------------------------------------
+  //=============================================================
+  // Methods I've developed.
+  //=============================================================
   
-  def getStats(filename : String) = {
+  //=============================================================
+  // Get the statistics for the file.
+  //=============================================================
+  
+  def getStats(filename : String, target : String) = {
     
-    val s = HashMap.empty[String,HashSet[String]]
+    val data = ListBuffer.empty[Array[String]]
+    val set = HashMap.empty[String,HashSet[String]]
+
     val src = Source.fromFile(filename)
     val itr = src.getLines()
-   
-    // Create a new HashSet for each label.
-    
     val attr = itr.next().split(",")
     
-    for(item <- attr) {
-      s+= (item -> new HashSet[String])
-    }
-
-    // Count the sub-attributes using the HashSet.
-    
-    val counts = new Array[Integer](attr.length)
-    var r : Integer = 0
-    
     var spl = Array[String]()
-    
-    for(line <- itr) {
-      
-      spl = line.split(",")
-      
-      for( ind <- 0 to spl.length - 1) {
-        
-        if(!s(attr(ind)).contains(spl(ind))) {
-          s(attr(ind)) += (spl(ind))
-          counts(ind) += 1
-        }
-        
+    var s = new Array[Integer](attr.length)
+    var c : Integer = 0
+
+    // Create a new HashSet for each attribute, except the target.
+
+    for(word <- attr) {
+      if(!word.equalsIgnoreCase(target)) {
+        set+= (word -> new HashSet[String])
       }
-      
-      r = r + 1
     }
     
-    // Determine how many of the sub-attributes are valid.
+    data += attr // This is used as a reference when building the training matrix.
+
+    // Get the count of sub-attributes using the HashSet.
+
+    for(line <- itr) {
+      spl = line.split(",")
+      data += spl
+      
+      for( col <- 0 to spl.length - 1) {
+        if(set.contains(attr(col))) {
+          if(!set(attr(col)).contains(spl(col))) {
+            set(attr(col)) += (spl(col))
+            s(col) = s(col) + 1
+          }
+        }
+      }
+    }
     
-    var c : Integer = 0
+    // Count the number of valid subattributes.
     
-    for( ind <- 0 to counts.length-1) {
-      if(counts(ind) <= 20) {
+    for(item <- set.valuesIterator) {
+      if(item.size <= 20) {
         c += 1
       }
     }
+    
+    c += 1 // This is the target variable.
 
-    (counts,r,c)
+    src.close()
+    
+    (data,s,c)
     
   }
   
-  def getSubSet(m : Iterator[ArrayBuffer[String]], col : Integer) = {
+  //=============================================================
+  // Get the subsets of a column.
+  //=============================================================
+  
+  def getSubSet(data : ListBuffer[Array[String]], col : Integer) = {
     
-    // Build subsets of data within a column.
-    // This set doesn't include the column.
+    val subSet = HashMap.empty[String,ListBuffer[Array[String]]]
+    var tmp = Array[String]()
+    var i : Integer = 0
     
-    val subSet = HashMap.empty[String,ArrayBuffer[ArrayBuffer[String]]]
-    var tmp = ArrayBuffer[String]()
+    // Build subsets of the data within a column. This set doesn't include the column.
     
-    for(item <- m) {
+    for(item <- data) {
 
+      // Create a ListBuffer for each sub-attribute of a column.
+      
       if(!subSet.contains(item(col))) {
-        subSet += (item(col) -> new ArrayBuffer[ArrayBuffer[String]]())
+        subSet += (item(col) -> new ListBuffer[Array[String]]())
       }
       
-      tmp = new ArrayBuffer[String]()
+      // Add items to an array, except the values within col.
+
+      tmp = new Array[String](item.length-1)
       
-      for(ind <- 0 to item.length-1) {
-        if(ind != col) {
-          tmp += item(ind)
+      i = 0
+      for(c <- 0 to item.length-1) {
+        if(c != col) {
+          tmp(i) = item(c)
+          i += 1
         }
       }
       
+      // Add the array to the ListBuffer.
       subSet(item(col)) += tmp
     }
 
+    printData(subSet)
+    
     (subSet.valuesIterator)
     
   }
   
-  def printMatrix(matrix : ArrayBuffer[ArrayBuffer[String]]) {
+  //=============================================================
+  // Calculate frequencies.
+  //=============================================================
+  
+  def calcFreq(data : ListBuffer[Array[String]]) = {
+    val freq = HashMap.empty[String,HashMap[String,Integer]] // Conditional
+    val tot = HashMap.empty[String,Integer] // Attribute
+    var tmp : String = ""
+    var sum : Integer = 0 // Total records
     
-    for( row <- 0 to matrix.length-1) {
-      
-      for( col <- 0 to matrix(row).length-1) {
+    for( item <- data) {
+      for(col <- 0 to item.size - 2) {
+          
+        if(!freq.contains(item(col))) {
+          freq += (item(col) -> HashMap.empty[String,Integer])
+        }
+
+        tmp = item(col)+""+item(item.size-1)
         
-        print(matrix(row)(col)+" ")
+        if(freq(item(col)).contains( tmp ) ) {
+          freq(item(col))( tmp ) += 1
+        } else {
+          freq(item(col)) += ( tmp -> 1)
+        } 
+        
+        if(!tot.contains(item(col))) {
+          tot += (item(col) -> 1)
+        } else {
+          tot(item(col)) += 1
+        }
+        
+      }
+      sum += 1
+    }
+    
+    printData(freq.valuesIterator)
+    for( item <- tot) {
+      println(item)
+    }
+    System.out.println(sum)
+   
+    (freq,tot,sum)
+    
+  }
+
+  //=============================================================
+  // The following methods are used to debug the application.
+  //=============================================================
+  
+  /*
+  def printData(data : Array[Array[String]]) {
+    
+    // Print the contents of the training matrix.
+    
+    for( row <- 0 to data.length-1) {
+      
+      for( col <- 0 to data(row).length-1) {
+        
+        print(data(row)(col)+" ")
         
       }
       println()
       
+    }
+    
+  }*/
+  
+  def printData(data : ListBuffer[Array[String]]) {
+    
+    // Print the contents of the training matrix.
+    
+    for( item <- data) {
+      
+      for( col <- 0 to item.length-1) {
+        
+        print(item(col)+" ")
+        
+      }
+      println()
+      
+    }
+    
+  }
+  
+  def printData(data : HashMap[String,ListBuffer[Array[String]]]) {
+    
+    // Print the contents of the subset.
+    
+    for( item <- data) {
+      println(item._1)
+      for( array <- item._2) {
+        for(col <- 0 to array.length - 1) {
+          print(array(col)+" ")
+        }
+        println()
+      }
+      println()
+    }
+    
+  }
+  
+  def printData(data : Iterator[HashMap[String,Integer]]) {
+    
+    for( item <- data){
+      println(item)
     }
     
   }
--- UADecisionTreeTest.scala
+++ UADecisionTreeTest.scala
@@ -2,6 +2,8 @@
 import scala.io.Source
 import java.io.PrintWriter
 import java.io.File
+import scala.collection.mutable.HashMap
+import scala.collection.mutable.ArrayBuffer
 
 object UADecisionTreeTest {
   
@@ -9,32 +11,33 @@ object UADecisionTreeTest {
     
     getRandomAccuracy("./weather.csv")
     
-    var t = new UADecisionTree()
+    val t = new UADecisionTree()
     t.setTreeMaxDepth(10)
-    val data = t.getTrainingMatrix("./train.csv", "GOLF")
     
-    var a = t.getSubSet(data.iterator, 0)
+    val data = t.getTrainingMatrix("./weather.csv", "PLAY GOLF")
     
-    for(item <- a) {
-      println(item)
-    }
+    val sub = t.getSubSet(data, 0)
+    
+    val freq = t.calcFreq(sub.next())
+    
+    //val maxCol = getFirstSplit(parent,child)
 
-    // Find first attribute to split on,
-    // then use recursion to split on data.
-    
-    //t.train(data,0)
-    
+    //t.train(data.iterator,0)
     
   }
   
-  //--------------------------------------------------
+  //=============================================================
+  // The necessary methods.
+  //=============================================================
+  
+  //=============================================================
   // Split the data into training data & test data.
   //
   // This approach is similiar to cluster sampling.
   // Every five lines is a cluster. We take one
   // line at random from that cluster. This produces
   // a rough 80/20 split.
-  //--------------------------------------------------
+  //=============================================================
   
   def getRandomAccuracy(filename : String) {
     
@@ -91,6 +94,57 @@ object UADecisionTreeTest {
     // or the max depth.
     
     // Perform this test 100 times & print the average test results.
+    
+  }
+  
+  //=============================================================
+  // Methods I've developed.
+  //=============================================================
+  
+  def getFirstSplit(parent : (HashMap[Int,HashMap[String,Integer]],Integer), child : (HashMap[String,HashMap[String,Integer]],HashMap[String,Integer])) : Integer = {
+    
+    var cIG : Double = 0
+    var maxIG : Double = 0
+    var maxEnt : Double = 0
+    var maxCol : Integer = 0
+    
+    var pProb : Double = 0
+    var entParent : Double = 0
+    
+    var cProb : Double = 0
+    var entChild : Double = 0
+    var entChildSum : Double = 0
+
+    for(key <- parent._1.keySet) {
+
+      entParent = 0
+      entChildSum = 0
+      for(p <- parent._1(key)) {
+        pProb = p._2.toDouble / parent._2
+        entParent = entParent + pProb * (scala.math.log10(pProb)/scala.math.log10(2))
+        
+        entChild = 0
+        for(c <- child._1(p._1)) {
+          cProb = c._2.toDouble / p._2
+          entChild = entChild + cProb * (scala.math.log10(cProb)/scala.math.log10(2))
+          
+        }
+        entChild = pProb * entChild * -1
+        entChildSum += entChild
+        
+      }
+      
+      entParent = entParent * -1
+      cIG = entParent - entChildSum;
+
+      if(cIG > maxIG) {
+        maxIG = cIG
+        maxCol = key
+      }
+      
+    }
+
+    return maxCol
     
   }
   
